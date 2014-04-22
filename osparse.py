ad4be63e32e19849e0a497f5e57ed06304b86503
Will Pennell <wpennell@gmail.com>
2014-04-21 23:29:33 -0400

    nmap filtering script

---
+++ osparse.py
@@ -0,0 +1,26 @@
+import xml.etree.ElementTree as ET
+tree = ET.parse('test.xml')
+root = tree.getroot()
+"""
+list = [
+		{'path': 'address', 'element': 'addr'},
+		{'path': 'hostnames/hostname', 'element': 'name'},
+		{'path': 'os/osmatch', 'element': 'name'}
+]
+
+for host in root.iter('host'):
+	for i in list:
+		item = host.find(i['path'])
+		if item is not None: 
+			print (i['path']), ':', item.get(i['element'])
+		else:
+			print (i['path']), ": UNKNOWN"
+	print "\n"
+	
+"""
+
+for host in root.iter('host'):
+	address = host.findall('address')
+	hostname = host.find('hostnames/hostname')
+	os = host.find('os/osmatch')
+	print address[0].get('addr'), hostname.get('name'), os.get('name')
