c78e2a08ac3fe431583fddc2503cee13084df280
Will Pennell <Eid010n@users.noreply.github.com>
2014-04-23 09:22:02 -0400

    Update multi-dns.py

--- multi-dns.py
+++ multi-dns.py
@@ -1,3 +1,9 @@
+# This is a script to solve an issue with a customer's DNS
+# It takes an IP as an imput, and queries multiple nameservers
+# with a reverse lookup to see what each one believes to be the
+# hostname.
+# --Eid010n
+
 from dns import resolver, reversename
 import argparse
 
@@ -9,15 +15,15 @@ dns_servers = {'cde': '8.8.8.8', 'prod': '8.8.4.4'}
 resolvers = {}
 
 def new_resolver (dns_ip):
-#takes the IP address of the specific DNS server as an argument	
-#returns a resolver object
+# takes the IP address of the specific DNS server as an argument	
+# returns a resolver object
 	temp = resolver.Resolver('configure=False')
 	temp.nameservers = [dns_ip]
 	return temp
 
 def build_resolvers():
-#builds a list of resolvers to use for DNS lookup
-#stores in global dictionary resolvers
+# builds a list of resolvers to use for DNS lookup
+# stores in global dictionary resolvers
 	for zone in dns_servers :
 		resolvers[zone] = new_resolver(dns_servers[zone])
 	return
